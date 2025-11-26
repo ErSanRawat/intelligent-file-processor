237d1041afcaa7d9cf78161f212e0eff484ec4a6
SanRawat <ersandeeprawat7@gmail.com>
2025-11-26 20:03:23 +0530

    Initial project skeleton

---
+++ src/main/java/com/sanrawat/intelligent_file_processor/IntelligentFileProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.sanrawat.intelligent_file_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentFileProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentFileProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanrawat/intelligent_file_processor/IntelligentFileProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanrawat.intelligent_file_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentFileProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
